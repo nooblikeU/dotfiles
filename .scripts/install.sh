b6b771eaaa42ecd75aee037979ae6d93e0598946
nooblikeU <unnamed444@protonmail.com>
2022-09-07 18:28:22 +0900

    Edited install.sh

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -8,12 +8,12 @@ mv dotfiles .dotfiles
 cp -r .dotfiles/. $HOME
 rm -rf fonts
 cd ~/.dotfiles/fonts
-cp "Hack Regular Nerd Font Complete.ttf" /usr/share/fonts
+sudo cp "Hack Regular Nerd Font Complete.ttf" /usr/share/fonts
 cd ~/.dotfiles
-pacman -S --needed -y $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
+sudo pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
 
 #part2
-sudo pacman -S --needed -y base-devel
+sudo pacman -S --needed base-devel
 cd ~/.src
 git clone https://aur.archlinux.org/paru.git
 cd paru
@@ -21,8 +21,8 @@ chmod 777 .
 makepkg -si
 
 #part3
-#cd ~/.dotfiles
-#paru -S --needed --noconfirm - < foreignpkglist.txt
+cd ~/.dotfiles
+paru -S --needed --noconfirm - < foreignpkglist.txt
 
 #part4 
 cd ~/.src
@@ -32,7 +32,7 @@ cp ~/.dotfiles/.zshrc $HOME
 
 #part5
 
-cp ~/.dotfiles/.scripts/dwmblocks_scripts/. /usr/local/sbin
+sudo cp ~/.dotfiles/.scripts/dwmblocks_scripts/. /usr/local/sbin
 
 git clone https://github.com/nooblikeU/dwm.git ~/.src
 sudo make -C ~/.src/dwm clean install
@@ -45,4 +45,3 @@ sudo make -C ~/.src/dwmblocks clean install
 
 #part6
 systemctl enable ly.service
-
